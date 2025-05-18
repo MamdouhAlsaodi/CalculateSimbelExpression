6f14f54e975c9981352f2e35ad375d7a4db9913c
Mamdouh Alsaudi <159956773+MamdouhAlsaodi@users.noreply.github.com>
2025-05-18 17:40:54 -0300

    Add files via upload

--- clsUtil.h
+++ clsUtil.h
@@ -1,79 +1,88 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <cmath>
+#include "clsExpressionEvaluator.h"
 using namespace std;
 
-
-class clsUtil 
+class clsUtil
 {
 public:
-    
-    static string NumberToText(int Number)
-    {
 
-        if (Number == 0)
-        {
-            return "";
+    static string NumberToText(int Number) {
+        if (Number == 0) return "zero ";
+
+        if (Number < 0)
+            return "menos " + NumberToText(-Number);
+
+        if (Number >= 1 && Number <= 19) {
+            string arr[] = { "", "um","dois","três","quatro","cinco","seis","sete",
+                "oito","nove","dez","onze","doze","treze","quatorze",
+                "quinze","dezesseis","dezessete","dezoito","dezenove" };
+            return arr[Number] + " ";
         }
 
-        if (Number >= 1 && Number <= 19)
-        {
-            string arr[] = { "", "One","Two","Three","Four","Five","Six","Seven",
-        "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen",
-          "Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" };
-
-            return  arr[Number] + " ";
-
+        if (Number >= 20 && Number <= 99) {
+            string arr[] = { "", "", "vinte","trinta","quarenta","cinquenta","sessenta",
+                "setenta","oitenta","noventa" };
+            return arr[Number / 10] + (Number % 10 ? " e " + NumberToText(Number % 10) : " ");
         }
 
-        if (Number >= 20 && Number <= 99)
-        {
-            string arr[] = { "","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
-            return  arr[Number / 10] + " " + NumberToText(Number % 10);
-        }
+        if (Number == 100)
+            return "cem ";
 
         if (Number >= 100 && Number <= 199)
-        {
-            return  "One Hundred " + NumberToText(Number % 100);
-        }
+            return "cento " + NumberToText(Number % 100);
 
         if (Number >= 200 && Number <= 999)
-        {
-            return   NumberToText(Number / 100) + "Hundreds " + NumberToText(Number % 100);
-        }
+            return NumberToText(Number / 100) + "centos " + NumberToText(Number % 100);
 
         if (Number >= 1000 && Number <= 1999)
-        {
-            return  "One Thousand " + NumberToText(Number % 1000);
-        }
+            return "mil " + NumberToText(Number % 1000);
 
         if (Number >= 2000 && Number <= 999999)
-        {
-            return   NumberToText(Number / 1000) + "Thousands " + NumberToText(Number % 1000);
-        }
+            return NumberToText(Number / 1000) + "mil " + NumberToText(Number % 1000);
 
         if (Number >= 1000000 && Number <= 1999999)
-        {
-            return  "One Million " + NumberToText(Number % 1000000);
-        }
+            return "um milhão " + NumberToText(Number % 1000000);
 
         if (Number >= 2000000 && Number <= 999999999)
-        {
-            return   NumberToText(Number / 1000000) + "Millions " + NumberToText(Number % 1000000);
-        }
+            return NumberToText(Number / 1000000) + "milhões " + NumberToText(Number % 1000000);
 
         if (Number >= 1000000000 && Number <= 1999999999)
-        {
-            return  "One Billion " + NumberToText(Number % 1000000000);
-        }
-        else
-        {
-            return   NumberToText(Number / 1000000000) + "Billions " + NumberToText(Number % 1000000000);
-        }
+            return "um bilhão " + NumberToText(Number % 1000000000);
 
-
+        return NumberToText(Number / 1000000000) + "bilhões " + NumberToText(Number % 1000000000);
     }
+
+
+    static string NumberToText( long double Number) {
+        int intPart = static_cast<long long>(Number);
+        double fracPart = Number - intPart;
+
+        string result = NumberToText(intPart);
+
+        if (fracPart > 0.00001) {
+            result += "vírgula ";
+
+            // استخراج الأرقام بعد الفاصلة
+            string fracStr = to_string(fracPart).substr(2); // بعد "0."
+
+            // إزالة الأصفار الزائدة في النهاية
+            while (!fracStr.empty() && fracStr.back() == '0')
+                fracStr.pop_back();
+
+            for (char c : fracStr)
+                result += NumberToText(static_cast<int>(c - '0'));
+
+        }
+
+        return result;
+    }
+
+
     static string GetTestExpression(int index) {
-        string expressions[15] = {
+        string expressions[19] = {
             "2 + 3 * 4",
             "10 / 2 + 7",
             "(8 + 2) * 5",
@@ -88,11 +97,48 @@ public:
             "(20 + 5) / 5",
             "9 * (3 + 7) - 50",
             "2 + 3 * (4 ^ 2 - 1)",
-            "((10 * 5 / 2 + 5 ) / 10) ^ 2"
+            "((10 * 5 / 2 + 5 ) / 10) ^ 2",
+            " 5 * -3",
+            "7 + -2",    
+            "(4 + -1) * 5",
+            "- 5 + 3"
         };
 
         return expressions[index];
-    
+
+    }
+    static void Start() {
+        string expression;
+        clsExpressionEvaluator<long double> evaluator;
+
+        cout << "\t\t\tCalcletar\n\n";
+
+        for (int i = 0; i < 15; i++) {
+            cout << endl << GetTestExpression(i) << endl;
+            long double result = evaluator.Evaluate(GetTestExpression(i));
+            cout << "Result: " << result << endl;
+            cout << NumberToText(result) << endl;
+        }
+
+        char Anser = 'y';
+        while (Anser == 'y') {
+
+            cout << "\nEnter a math expression: ";
+            getline(cin, expression);
+
+            try {
+                long double result = evaluator.Evaluate(expression);
+                cout << "\nResult: " << result << endl;
+                cout << NumberToText(result) << endl;
+            }
+            catch (const exception& ex) {
+                cout << "\nError: " << ex.what() << endl;
+            }
+            cout << "\nDo you want to continue? y/n ";
+            cin >> Anser;
+            cin.ignore();
+        }
+
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,46 +1,14 @@
 #include <iostream>
 #include <string>
 #include "clsExpressionEvaluator.h"
-#include"clsUtil.h"
+#include "clsUtil.h"
 
 using namespace std;
 
 
-void Start() {
-    string expression;
-    clsExpressionEvaluator<long double> evaluator;
-
-    cout << "\t\t\tCalcletar\n\n";
-
-    for (int i = 0; i < 15; i++) {
-            cout << endl << clsUtil::GetTestExpression(i) << endl;
-            long double result = evaluator.Evaluate(clsUtil::GetTestExpression(i));
-            cout << "Result: " << result << endl; 
-    }
-
-    char Anser = 'y'; 
-    while (Anser == 'y')  { 
-        
-          cout << "\nEnter a math expression: ";
-        getline(cin, expression);
-
-        try {
-            long double result = evaluator.Evaluate(expression);
-            cout << "\nResult: " << result << endl;
-        }
-        catch (const exception& ex) {
-            cout << "\nError: " << ex.what() << endl;
-        }
-        cout << "\nDo you want to continue? y/n ";
-        cin >> Anser;
-        cin.ignore();
-    }
-        
-}
-    
 
 int main() {
-    Start();
+    clsUtil::Start();
     return 0;
 }
 
